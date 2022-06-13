49d045934e6ee9595b04ec63126dcb9bc54467a9
Dele Onabowu <delesodade@hotmail.com>
2022-06-13 22:52:11 +0100

    New update

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "eu-west-2"
 }
 
-# Create VPC
+# Create VPC in AWS
 resource "aws_vpc" "main" {
   cidr_block                     = "172.16.0.0/16"
   enable_dns_support             = "true"
