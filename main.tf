13e9db76918edfff16589c93a1a400002a59ce96
Dele Onabowu <delesodade@hotmail.com>
2022-06-13 23:57:37 +0100

    Updated terraform code. VPC with subnets

--- main.tf
+++ main.tf
@@ -14,3 +14,28 @@ resource "aws_vpc" "main" {
     Name = "delesVPC"
   }
 }
+
+# Create public subnets1
+resource "aws_subnet" "public1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "172.16.0.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "eu-west-2a"
+
+  tags = {
+    Name = "public-subnet1"
+  }
+
+}
+
+# Create public subnet2
+resource "aws_subnet" "public2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "172.16.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "eu-west-2b"
+
+  tags = {
+    Name = "public-subnet2"
+  }
+}
