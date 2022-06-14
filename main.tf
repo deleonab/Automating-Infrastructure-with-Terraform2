b331f7943c4cedf27fb8142fab29d4c363e0b44a
Dele Onabowu <delesodade@hotmail.com>
2022-06-14 19:32:21 +0100

    Refactored Terraform code for VPC, Subnets and security groups

--- main.tf
+++ main.tf
@@ -1,41 +1,92 @@
-provider "aws" {
-  region = "eu-west-2"
+# Get list of availability zones
+        data "aws_availability_zones" "available" {
+        state = "available"
+        }
+
+variable "region" {
+        default = "eu-west-2"
+    }
+
+
+
+variable "vpc_cidr" {
+        default = "172.16.0.0/16"
+    }
+
+variable "enable_dns_support" {
+        default = "true"
+    }    
+
+
+variable "enable_dns_hostnames" {
+        default ="true" 
+    }
+
+
+variable "enable_classiclink" {
+        default = "false"
+    }
+
+variable "enable_classiclink_dns_support" {
+        default = "false"
+    }
+
+
+# Declare a variable to store the desired number of public subnets, and set the default value
+variable "preferred_number_of_public_subnets" {
+  default = 2
 }
+
+provider "aws" {
+  region = var.region    
+   }
+
+
+  
 
 # Create VPC
 resource "aws_vpc" "main" {
-  cidr_block                     = "172.16.0.0/16"
-  enable_dns_support             = "true"
-  enable_dns_hostnames           = "true"
-  enable_classiclink             = "false"
-  enable_classiclink_dns_support = "false"
+  cidr_block                     = var.vpc_cidr
+  enable_dns_support             = var.enable_dns_support
+  enable_dns_hostnames           = var.enable_dns_hostnames
+  enable_classiclink             = var.enable_classiclink
+  enable_classiclink_dns_support = var.enable_classiclink_dns_support
 
-  tags = {
-    Name = "delesVPC"
   }
-}
 
 # Create public subnets1
-resource "aws_subnet" "public1" {
+resource "aws_subnet" "public" {
+  count = var.preferred_number_of_public_subnets == null ? length(data.aws_availability_zones.available.names) : var.preferred_number_of_public_subnets
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "172.16.0.0/24"
+  cidr_block              = cidrsubnet(var.vpc_cidr,4,count.index)
   map_public_ip_on_launch = true
-  availability_zone       = "eu-west-2a"
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
 
   tags = {
-    Name = "public-subnet1"
+    Name = "public-subnet"
   }
 
 }
 
-# Create public subnet2
-resource "aws_subnet" "public2" {
+
+
+# Create private subnet4
+resource "aws_subnet" "private4" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "172.16.1.0/24"
+  cidr_block              = "172.16.6.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "eu-west-2b"
 
   tags = {
-    Name = "public-subnet2"
+    Name = "private-subnet4"
   }
 }
+
+
+
+
+
+
+
+
+
