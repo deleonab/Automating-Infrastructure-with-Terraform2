4fe9d74bbb8d12d15b40aac2f4890187c8c07ecf
Dele Onabowu <delesodade@hotmail.com>
2022-07-28 00:11:44 +0100

    terraform infrastructure as code documentation updated

--- variables.tf
+++ variables.tf
@@ -38,3 +38,9 @@ variable "preferred_number_of_public_subnets" {
 variable "preferred_number_of_private_subnets" {
   type = number
 }
+
+variable "tags" {
+  description = "A mapping of tags to assign to all resources."
+  type        = map(string)
+  default     = {}
+}
