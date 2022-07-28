c637087916d0721c0bb343c43cb75d6abaaba4b5
Dele Onabowu <delesodade@hotmail.com>
2022-07-28 22:35:19 +0100

    terraform infrastructure as code documentation updated

--- main.tf
+++ main.tf
@@ -81,7 +81,7 @@ resource "aws_eip" "nat_eip" {
 resource "aws_nat_gateway" "ngw" {
   allocation_id = aws_eip.nat_eip.id
   subnet_id     = element(aws_subnet.public.*.id,0)
-  depends_on = [aws_internet_gateway.ngw]
+  depends_on = [aws_internet_gateway.igw]
  tags = merge(
     var.tags,
     {
