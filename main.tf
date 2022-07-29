c857c22718755fa15968c90e21048a314e042b3b
Dele Onabowu <delesodade@hotmail.com>
2022-07-29 20:29:17 +0100

    terraform infrastructure as code documentation updated

--- main.tf
+++ main.tf
@@ -121,7 +121,7 @@ resource "aws_route_table_association" "private_subnet_assoc" {
     
     count = length(aws_subnet.private[*].id)
     subnet_id = element(aws_subnet.private[*].id, count.index)
-    route_table_id = aws_route_table.private_rtb.id
+    route_table_id = aws_route_table.private-rtb.id
   }
 
 
