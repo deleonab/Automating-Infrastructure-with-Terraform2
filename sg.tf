d5608732e27ff65714b3159989b758e94a8e0e5c
Dele Onabowu <delesodade@hotmail.com>
2022-07-30 22:20:54 +0100

    Added IAM Roles to template

--- sg.tf
+++ sg.tf
@@ -4,22 +4,7 @@ resource "aws_security_group" "ext-alb-sg" {
   vpc_id      = aws_vpc.main.id
   description = "Allow TLS inbound traffic"
 
-  ingress {
-    description = "HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "HTTPS"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
+  
   egress {
     from_port   = 0
     to_port     = 0
