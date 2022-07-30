cea8ce345262423a567029782380977f797b80ef
Dele Onabowu <delesodade@hotmail.com>
2022-07-30 11:21:50 +0100

    terraform infrastructure as code documentation updated

---
+++ albexternal.tf
@@ -0,0 +1,26 @@
+# We need to create an ALB to balance the traffic between the Instances:
+
+resource "aws_lb" "ext-alb" {
+  name     = "ext-alb"
+  internal = false
+  security_groups = [
+    aws_security_group.ext-alb-sg.id,
+  ]
+
+  subnets = [
+    aws_subnet.public[0].id,
+    aws_subnet.public[1].id
+  ]
+
+   tags = merge(
+    var.tags,
+    {
+      Name = "ACS-ext-alb"
+    },
+  )
+
+  ip_address_type    = "ipv4"
+  load_balancer_type = "application"
+}
+
+# We need to inform the ALB of where where route the traffic.  We need to create a Target Group for our load balancer
