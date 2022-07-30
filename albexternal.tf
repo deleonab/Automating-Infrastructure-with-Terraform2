40d25208d873227a5c41ac9590d373cd7fc9fa69
Dele Onabowu <delesodade@hotmail.com>
2022-07-30 11:45:28 +0100

    terraform infrastructure as code documentation updated

--- albexternal.tf
+++ albexternal.tf
@@ -24,3 +24,38 @@ resource "aws_lb" "ext-alb" {
 }
 
 # We need to inform the ALB of where where route the traffic.  We need to create a Target Group for our load balancer
+# Create the target group
+# The targets are our nginx reverse proxy servers
+
+resource "aws_lb_target_group" "nginx-tgt" {
+  health_check {
+    interval            = 10
+    path                = "/healthstatus"
+    protocol            = "HTTPS"
+    timeout             = 5
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+  }
+  name        = "nginx-tgt"
+  port        = 443
+  protocol    = "HTTPS"
+  target_type = "instance"
+  vpc_id      = aws_vpc.main.id
+}
+
+
+# Next, we will create a Listner for the target group aws_lb_target_group.nginx-tgt
+
+resource "aws_lb_listener" "nginx-listner" {
+  load_balancer_arn = aws_lb.ext-alb.arn
+  port              = 443
+  protocol          = "HTTPS"
+  certificate_arn   = aws_acm_certificate_validation.oyindamola.certificate_arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.nginx-tgt.arn
+  }
+}
+
+
