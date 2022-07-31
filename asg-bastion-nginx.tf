02a7d5fa2342d57a87dd81e4743b8bbfa0743919
Dele Onabowu <delesodade@hotmail.com>
2022-07-31 09:39:12 +0100

    Latest changes to code

--- asg-bastion-nginx.tf
+++ asg-bastion-nginx.tf
@@ -38,7 +38,7 @@ resource "aws_launch_template" "bastion-launch-template" {
   key_name = var.keypair
 
   placement {
-    availability_zone = "random_shuffle.az_list.result"
+    availability_zone = "${random_shuffle.az_list.result}"
   }
 
   lifecycle {
